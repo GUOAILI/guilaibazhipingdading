3516658c88062d5d59b343639355d5cb81bcbc67
zhengdaozhao <guloujie@gmail.com>
2025-05-12 09:34:18 +0800

    pagiantion remaining files

--- zackdson/src/form/NotebookList.jsx
+++ zackdson/src/form/NotebookList.jsx
@@ -1,165 +1,168 @@
-import React, { useEffect,useState,Fragment } from "react";
-import {useNavigate,redirect} from 'react-router-dom';
+import React, { useEffect, useState, Fragment } from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
 import {
-  Button,Table,
+  Button, Table,
   Spin,
-  notification,Popconfirm
+  notification, Popconfirm
 } from 'antd';
 import TableService from "../util/tableService";
 
-const openNotificationWithIcon = (type, message, description) => notification[type]({message, description});
+const openNotificationWithIcon = (type, message, description) => notification[type]({ message, description });
 
 function NotebookList() {
-    const navigate = useNavigate();
-    const [user, setUser]=useState([]);
-    const [xiaofang,setXiaofang]=useState(false);
-    const [isLoading, setIsLoading]=useState(false);
-    const subject=localStorage.getItem("branchDetail");
+  const navigate = useNavigate();
+  const [user, setUser] = useState([]);
+  const [xiaofang, setXiaofang] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const subject = localStorage.getItem("branchDetail");
+  // 2025/5/12 handle return navigation
+  const [currentPage, setCurrentPage] = useState(1);
+  const location = useLocation();
 
-    const deleteOneRecord = async (id)=>{
-      try{
-        await TableService.delOneNotebook(id);
-        setXiaofang(x=>!x);
-        openNotificationWithIcon("success","删除课本记录成功");
-      }catch(ex){
-        openNotificationWithIcon("error","删除课本记录异常,请联系管理员");
-      }
+  const deleteOneRecord = async (id) => {
+    try {
+      await TableService.delOneNotebook(id);
+      setXiaofang(x => !x);
+      openNotificationWithIcon("success", "删除记录成功");
+    } catch (ex) {
+      openNotificationWithIcon("error", "删除记录异常,请联系管理员");
     }
-    const editRecord = (record)=>{
-      localStorage.setItem("notebookRecord",JSON.stringify(record));
-      navigate('/nav/notebook/edit');
-    }
+  }
+  const editRecord = (record) => {
+    localStorage.setItem("notebookRecord", JSON.stringify(record));
+    navigate('/nav/notebook/edit');
+  }
 
-    const columns = [
-        {
-          title: '目录(可点击)',
-          dataIndex: 'num',
-          key: 'num',
-          // render: (text) => <span style={{color:'red'}}>{text}</span>,
-          // 跳转详情页
-          render: (text, record) => {
-            
-            return <a onClick={()=>{
-                localStorage.setItem("notebookRecord",JSON.stringify(record));
-                navigate('/nav/notebook/detail');
-            }}>
-                第{text}课
-            </a>
-            }
-        },
-        {
-          title: '主题',
-          dataIndex: 'keyword',
-          key: 'keyword',
-        },        {
-          title: '照片',
-          // dataIndex: 'mjddyz',
-          key: 'photo',
-          render: (_,record) => (<span> {record.mjddyz.length>0 ? record.mjddyz.length+'张' : '未添加'} </span>),
-        },
-        {
-          title: '做成日',
-          dataIndex: 'beginday',
-          key: 'beginday',
-          render: (text, record) => {
-            if (record.beginday !== record.modday) {
-              return (
-                <span>
-                  <span style={{ color: '#1890ff' }}>做成:</span> {record.beginday}
-                  <br />
-                  <span style={{ color: '#faad14' }}>修正:</span> {record.modday}
-                </span>
-              );
-            }
-            return record.beginday;
-          }
-        },
-        {
-          title: '难易度',
-          dataIndex: 'easy',
-          key: 'easy',
-          render: (text) => {
-            if (text === '高') {
-              return <span style={{ color: '#d0021b', fontSize:'1.8em' }}>{text}</span>;
-            }
-            if (text === '中') {
-              return <span style={{ color: '#1890ff',fontSize:'1.2em' }}>{text}</span>;
-            }
-            return <span style={{ color: 'gray', fontWeight: 'bold' }}>{text}</span>;
-          }
-        },
-        {
-          // reuse the perfect code of lagacy project fujitsu
-          title: 'Action',
-          className:'laoyaoziling',
-          key: 'action',
-          width: 160, // 可根据实际调整
-          render: (text, record) => (
-            <Fragment>
-              <Popconfirm
-                title={`删除 第${record.num}课记录`} 
-                description="你确定真的要删除吗?"
-                onConfirm={() => deleteOneRecord(record.id)} okText='确定' cancelText='取消'
-              >
-                <Button type="primary" danger style={{fontSize:'12px'}}>删除</Button>
-              </Popconfirm>
-
-              <Button style={{marginLeft: '5px',backgroundColor:'green',fontSize:'12px'}} type='primary' onClick={() => editRecord(record)}>修改</Button>
-            </Fragment>
-          ),
-        }                
-];
-
-  useEffect( ()=>{
-    const zpddyz = async ()=> {
-        try{
-          setIsLoading(true);
-          const res = await TableService.getAllNotebook(subject);
-          // console.log(res.data);
-          setUser(res.data);
-          setIsLoading(false);
-        } catch(err){
-          setIsLoading(false);
-          openNotificationWithIcon("error","获取后台课本数据出错,请联系管理员")
-          // setIsLoading(true);
-          // console.log(err);
+  const columns = [
+    {
+      title: '标题(可点击)',
+      dataIndex: 'title',
+      key: 'title',
+      render: (text, record) => (
+        <a onClick={() => {
+          localStorage.setItem("notebookRecord", JSON.stringify(record));
+          navigate('/nav/notebook/detail', { state: { pageNumber: currentPage } });
+        }}>
+          {text}
+        </a>
+      )
+    },
+    {
+      title: '照片',
+      key: 'photo',
+      render: (_,record) => (<span> {record.mjddyz.length>0 ? record.mjddyz.length+'张' : '未添加'} </span>),
+     },
+    {
+      title: '做成日',
+      dataIndex: 'beginday',
+      key: 'beginday',
+      render: (text, record) => {
+        if (record.beginday !== record.modday) {
+          return (
+            <span>
+              <span style={{ color: '#1890ff' }}>做成:</span> {record.beginday}
+              <br />
+              <span style={{ color: '#faad14' }}>修正:</span> {record.modday}
+            </span>
+          );
         }
+        return record.beginday;
+      }
+    },
+    {
+      title: '重要度',
+      dataIndex: 'imp',
+      key: 'imp',
+
+      render: (text) => {
+        if (text === '高') {
+          return <span style={{ color: '#d0021b', fontSize:'1.8em' }}>{text}</span>;
+        }
+        if (text === '中') {
+          return <span style={{ color: '#1890ff',fontSize:'1.2em' }}>{text}</span>;
+        }
+        return <span style={{ color: 'gray', fontWeight: 'bold' }}>{text}</span>;
+      }
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      width: 160, // 可根据实际调整
+      render: (text, record) => (
+        <Fragment>
+          <Popconfirm
+            title={`删除 ${record.title}`}
+            description="你确定真的要删除吗?"
+            onConfirm={() => deleteOneRecord(record.id)} okText='确定' cancelText='取消'
+          >
+            <Button type="primary" danger style={{ fontSize: '12px' }}>删除</Button>
+          </Popconfirm>
+          <Button style={{ marginLeft: '5px', backgroundColor: 'green', fontSize: '12px' }} 
+                  type='primary' 
+                  onClick={() => editRecord(record)}
+          >
+            修改
+          </Button>
+        </Fragment>
+      ),
+    }
+  ];
+
+  useEffect(() => {
+    const zpddyz = async () => {
+      try {
+        setIsLoading(true);
+        const res = await TableService.getAllNotebook(subject);
+        setUser(res.data);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        openNotificationWithIcon("error", "获取后台数据出错,请联系管理员")
+      }
     };
     zpddyz();
-  },[xiaofang]);
+
+    // 2025/5/12 handle return navigation
+    if (location.state?.returnPage) {
+      setCurrentPage(location.state.returnPage);
+    }
+  }, [xiaofang, location]);
 
   const getRowClassName = (_, index) => {
     let className = ''
-    // oddRow 和 evenRow为我们css文件中的样式名称
     className = index % 2 === 0 ? "oddRow" : "evenRow"
     return className
   }
 
   return (
     <>
-    <h1>{localStorage.getItem("branchDetail")}</h1>
-    <div style={{background:'white'}}>
-          {isLoading ?
+      <h1>{localStorage.getItem("branchDetail")}</h1>
+      <div style={{ background: 'white' }}>
+        {isLoading ?
           <>
             <h2>正在获取以往记录...</h2>
-            <Spin style={{marginLeft:'23rem'}} size="large"/> 
+            <Spin style={{ marginLeft: '23rem' }} size="large" />
           </>
-          : 
+          :
           <>
-            {/* <h2>光辉的足迹</h2> */}
-            <Table columns={columns} 
-              dataSource={user} 
-              rowClassName={getRowClassName}  
-              rowKey={rec=>rec.id} 
-              />
+            <Table columns={columns}
+              dataSource={user}
+              rowClassName={getRowClassName}
+              rowKey={rec => rec.id}
+              // 2025/5/12
+              pagination={{
+                current: currentPage,
+                onChange: (page) => setCurrentPage(page)
+              }}
+            />
           </>
-          }
-          <div style={{display:'flex',justifyContent:'center'}}>
-            <Button style={{width:'8rem',marginTop:'1rem',marginBottom:'2rem'}}  type="primary" onClick={()=>navigate('/nav/notebook/input')}>
-              我要追加
-            </Button>
-          </div>
-    </div>
+        }
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button style={{ width: '8rem', marginTop: '1rem', marginBottom: '2rem' }} type="primary" onClick={() => navigate('/nav/notebook/input')}>
+            我要追加
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
